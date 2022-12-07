0ad1873ff8841770521910fbb602da3ff66ed649
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2022-12-07 08:13:50 -0500

    Added exe 2 chap 1

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/gwyne/Developer/learning-rust/projects/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a: src/main.rs
+
+/mnt/c/Users/gwyne/Developer/learning-rust/projects/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/gwyne/Developer/learning-rust/projects/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/gwyne/Developer/learning-rust/projects/hello_cargo/src/main.rs
